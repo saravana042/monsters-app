9315289bd802dd53143c24377f929872ce4657bb
saravana042 <usaravanankarthick@gmail.com>
2020-04-26 08:39:32 +0800

    adding files in git hub

---
+++ build/precache-manifest.b9170e85ed9d177319e78012870400f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32577e3184978833cf1b85b757e5138f",
+    "url": "/monsters-app/index.html"
+  },
+  {
+    "revision": "49bbde7b97bd68656cef",
+    "url": "/monsters-app/static/css/main.82cd3443.chunk.css"
+  },
+  {
+    "revision": "f47f3b7ee81bdee07e68",
+    "url": "/monsters-app/static/js/2.fe422383.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-app/static/js/2.fe422383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49bbde7b97bd68656cef",
+    "url": "/monsters-app/static/js/main.1d3d193f.chunk.js"
+  },
+  {
+    "revision": "fe80a095cd5dca4857f1",
+    "url": "/monsters-app/static/js/runtime-main.d27421b0.js"
+  }
+]);
